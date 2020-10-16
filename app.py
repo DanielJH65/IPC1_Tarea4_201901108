3529930a51a56fe745d6e63cdafeb2ed853f59ce
Daniel <wdaniel.jimenez@gmail.com>
2020-10-15 22:06:54 -0600

    Crear ruta eliminar y modificar

--- app.py
+++ app.py
@@ -35,13 +35,49 @@ def ontenerFunciones():
     global peliculas
     for pelicula in peliculas:
         json_peliculas.append({
-            'titulos': pelicula.titulo, 
+            'titulo': pelicula.titulo, 
             'url_imagen' : pelicula.url_imagen, 
             'puntuacion': pelicula.puntuacion,
             'duracion' : pelicula.duracion,
             'sinopsis' : pelicula.sinopsis
             })
     return jsonify(json_peliculas)
+
+@app.route('/modificarPelicula', methods = ['POST'])
+def modificarPelicula():
+    datos = request.get_json()
+    titulo_actual = datos['titulo_actual']
+    titulo = datos['titulo']
+    url_imagen = datos['url_imagen']
+    puntuacion = datos['puntuacion']
+    duracion = datos['duracion']
+    sinopsis = datos['sinopsis']
+    global peliculas
+    for pelicula in peliculas:
+        if pelicula.titulo == titulo_actual:
+            for pelicula2 in peliculas:
+                if pelicula2.titulo == titulo:
+                    return jsonify({'mensaje': 'Error, ya existe en la lista de peliculas el nuevo nombre, prueba otro'}), status.HTTP_400_BAD_REQUEST
+            pelicula.titulo = titulo
+            pelicula.url_imagen = url_imagen
+            pelicula.puntuacion = puntuacion
+            pelicula.duracion = duracion
+            pelicula.sinopsis = sinopsis
+            return jsonify({'mensaje': 'Satisfactorio, la pelicula se modificó correctamente'})
+    return jsonify({'mensaje': 'Error, la pelicula no existe en la lista de peliculas'}), status.HTTP_400_BAD_REQUEST
+
+@app.route('/eliminarPelicula', methods = ['POST'])
+def eliminarPelicula():
+    datos = request.get_json()
+    titulo = datos['titulo']
+    global peliculas
+    i = 0
+    for pelicula in peliculas:
+        if pelicula.titulo == titulo:
+            peliculas.pop(i)
+            return jsonify({'mensaje': 'Satisfactorio, la pelicula se eliminó correctamente'})
+        i += 1
+    return jsonify({'mensaje': 'Error, la pelicula no existe en la lista de peliculas'}), status.HTTP_400_BAD_REQUEST
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port = '3000')
